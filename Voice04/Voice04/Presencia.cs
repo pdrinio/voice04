7b411896c12ef133f1a41163af598f42be7f5df6
Pedro Orgeira <poc.desarrollo@hotmail.com>
2017-09-19 23:13:28 +0200

    PRESENCIA: definición de objeto y métodos para controlar presencia

--- Voice04/Voice04/Presencia.cs
+++ Voice04/Voice04/Presencia.cs
@@ -1,23 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Voice04
 {
-    class Presencia
+    public class Presencia
     {
         public enum Habitantes { Pa, Ma, Cris, Pablo};
         public List<Habitantes> Personas; 
      
 
         public Presencia()
-        {           
+        {                  
             Personas.Add(Habitantes.Pa);
             Personas.Add(Habitantes.Ma);
             Personas.Add(Habitantes.Cris);
             Personas.Add(Habitantes.Pablo);
+        }
+
+        public void Add(Habitantes HabitanteEntrante)
+        {
+            if (!Personas.Contains(HabitanteEntrante))
+                Personas.Add(HabitanteEntrante);                  
+        }
+
+        public void Remove(Habitantes HabitanteSaliente)
+        {
+            if (Personas.Contains(HabitanteSaliente))
+                Personas.Remove(HabitanteSaliente);
+        }
+
+        public int Contains()
+        {
+            return Personas.Count();
         }
     }
 }
