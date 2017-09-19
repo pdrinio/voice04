9d1d0c1dd3acbe158c7b50b552532967bf9b665f
Pedro Orgeira <poc.desarrollo@hotmail.com>
2017-09-19 23:38:08 +0200

    CONVERSACIONES: definición de nodo

---
+++ Voice04/Voice04/Conversaciones/Nodo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voice04.Conversaciones
+{
+    class Nodo
+    {
+        public int IdNodo;
+        public enum TiposNodo { Pregunta, Afirmacion};
+        public TiposNodo TipoNodo;
+        public string MensajeIda;
+        public List<String> PalabrasIdentificativas;
+        public List<int> SiguientesNodos;
+
+        public Nodo(int IdNodo, TiposNodo TipoNodo, string MensajeIda, List<string> PalabrasIdentificativas, List<int> SiguientesNodos)
+        {
+            this.IdNodo = IdNodo;
+            this.TipoNodo = TipoNodo;
+            this.MensajeIda = MensajeIda;
+            this.PalabrasIdentificativas = PalabrasIdentificativas;
+            this.SiguientesNodos = SiguientesNodos;
+        }
+    }
+}
